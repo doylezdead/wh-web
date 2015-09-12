3d0b4997f3dd2a2a5a0f75da367c7721f74f52b2
Chris <crcopper@mtu.edu>
2015-09-12 18:43:04 -0400

    Please work

--- js/wordhunter.js
+++ js/wordhunter.js
@@ -4,4 +4,5 @@ $(document).ready(function(){
 	
 	var model = new searchModel();
 	var searchView = new SearchView({ model: model, el: '#mainContainer'});
+	$("#mainContainer").html(searchView.render().el);
 });
