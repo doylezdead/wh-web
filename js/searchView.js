df722afe360d1a6ddccfb3e36dda8dd38dcb968b
Chris <crcopper@mtu.edu>
2015-09-12 15:48:25 -0400

    Still fixin it

--- js/searchView.js
+++ js/searchView.js
@@ -6,7 +6,7 @@ var SearchView = Backbone.View.extend({
 	},
 	
 	render: function(){
-		this.$el.html(this.template());
+		this.$el.html(this.template);
 		$('body').append(this.$el);
         return this;
 	}
