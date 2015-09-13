32de5542cf72d2576c38fd9fcb56ed60f8749710
Chris <crcopper@mtu.edu>
2015-09-12 21:26:13 -0400

    Fixed error

--- js/searchView.js
+++ js/searchView.js
@@ -21,7 +21,7 @@ var SearchView = Backbone.View.extend({
 		
 		this.model.fetch({
 			wait: true,
-			success: function(){
+			success: function(model, response){
 				response = $.map(this.model.toJSON(), $.proxy(function(val, i){
 					return val;
 				}, this));		
