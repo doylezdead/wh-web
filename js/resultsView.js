ee1929c85af5dd632664a9d48447a91b119904bd
Chris <crcopper@mtu.edu>
2015-09-13 04:49:25 -0400

    CSS changes

--- js/resultsView.js
+++ js/resultsView.js
@@ -1,6 +1,11 @@
 var ResultsView = Backbone.View.extend({
 	template: Handlebars.compile( $("#resultsTemplate").html()),
-
+	
+	events: {
+		'click .upArrow' : 'toggleArrow',
+		'click .downArrow' : 'toggleArrow'
+	},	
+	
 	initialize: function(options){
 		this.response = options.response;
 	},
@@ -9,5 +14,19 @@ var ResultsView = Backbone.View.extend({
 		this.$el.html(this.template({response: this.response}));
         return this;
         
+	},
+	
+	toggleArrow: function(event){
+		target = $(event.target);
+		if(target.hasClass('upArrow')){
+			target.toggleClass('orange');
+			var parentID = target.parent().attr('id');
+			$(parentID + ' .downArrow').toggleClass('purple');
+		}
+		else if(target.hasClass('downArrow')){
+			target.toggleClass('purple');
+			var parentID = target.parent().attr('id');
+			$(parentID + ' .upArrow').toggleClass('orange');
+		}
 	}
 });
